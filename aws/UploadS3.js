f53c78d8890ec0df02f3a0d02b5bf865c764cb1f
Sahil Bhatt <ethenhunt556@gmail.com>
2024-01-07 15:36:42 +0530

    Update AWS configuration and use environment variables for access credentials

--- aws/UploadS3.js
+++ aws/UploadS3.js
@@ -1,7 +1,17 @@
-const { S3 } = require("aws-sdk");
+const AWS = require("aws-sdk");
+const dotenv = require("dotenv");
+dotenv.config();
+AWS.config.logger = console;
 
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
 exports.s3Uploadv3 = async (file) => {
-  const s3 = new S3();
+
   if (!file) {
     throw new Error("File is missing");
   }
