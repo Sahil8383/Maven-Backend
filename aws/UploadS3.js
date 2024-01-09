2eaba42154ccae517bb9178571e35fb402c81d54
AakashSingh20 <akashsingh20603@gmail.com>
2024-01-09 14:48:20 +0530

    stable, sahil dont fuck this one up

--- aws/UploadS3.js
+++ aws/UploadS3.js
@@ -4,14 +4,13 @@ dotenv.config();
 AWS.config.logger = console;
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
 const s3 = new AWS.S3();
 exports.s3Uploadv3 = async (file) => {
-
   if (!file) {
     throw new Error("File is missing");
   }
