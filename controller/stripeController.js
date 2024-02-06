0a8f2d030868a02da9bb71a0f672521d5fdbde4b
Sahil Bhatt <ethenhunt556@gmail.com>
2024-02-06 22:36:17 +0530

    Stripe payments route added

---
+++ controller/stripeController.js
@@ -0,0 +1,26 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+exports.createCheckoutSession = async (req, res) => {
+    const { amount } = req.body;
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+            {
+                price_data: {
+                    currency: "inr",
+                    product_data: {
+                        name: "Subscription Plan",
+                    },
+                    unit_amount: amount*100,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: "payment",
+        success_url: "http://localhost:3000/",
+        cancel_url: "http://localhost:3000/cancel",
+    });
+
+    res.json({ id: session.id });
+}
